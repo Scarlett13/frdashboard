d57855a666b295967602c4e5892dd21e682168ef
yudhapaderip97 <yudha@visiglobalteknologi.co.id>
2023-07-04 10:33:20 +0700

    perbaikan logo di background.tsx

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -7,17 +7,18 @@ type BacgroundProps = {
 
 export default function Bacground({ children, title }: BacgroundProps) {
   return (
-    // <div className="page">
-    <div className="min-h-screen bg-gray-50 flex flex-col justify-center items-center">
-      {/* <div className="max-w-md w-full mx-auto"> */}
-      <div className="text-3xl font-bold text-gray-900 mt-2 mb-9 text-center">
-        <Image alt="alt text." src={logo} width={120} height={100} />
-        <h1>{title}</h1>
-
-        <div>{children}</div>
+    <div className="page">
+      <div className="min-h-screen bg-gray-50 flex flex-col justify-center items-center">
+        <div className="max-w-md w-full mx-auto">
+          <div className="text-center flex justify-center">
+            <Image alt="alt text." src={logo} width={120} height={100} />
+          </div>
+          <div className="text-3xl font-bold text-gray-900 mt-2 mb-9 text-center">
+            <h1>{title}</h1>
+            <div>{children}</div>
+          </div>
+        </div>
       </div>
-      {/* </div> */}
     </div>
-    // </div>
   );
 }
