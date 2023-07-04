2f76a76c9c795654a478d5533a251bd5f9166d83
ctscope-development <development@ctscope.io>
2023-07-04 15:52:28 +0700

    fix: centered the logo
    Merge rebase conflict resolved

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,7 +3,7 @@ type ButtonProps = {
 };
 export default function Button({ buttonname }: ButtonProps) {
   return (
-    <button className="w-full py-2 px-4 bg-blue-600 hover:bg-blue-700 rounded-md text-white text-sm">
+    <button className="w-full py-2 px-4 bg-blue-600 hover:bg-blue-700 rounded-md text-white text-sm mt-4">
       {buttonname}
     </button>
   );
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,16 +1,12 @@
 import Card from "@/components/card";
 import Bacground from "@/components/background";
-import UserName from "@/components/username";
-import Password from "@/components/password";
 import Button from "@/components/button";
 import Input from "@/components/input";
+
 export default function Login() {
   return (
     <Card>
       <Bacground title={"Login"}>
-        {/* <UserName />
-        <Password /> */}
-
         <Input
           title={"Username/Email"}
           placeholder={"Username / Email"}
