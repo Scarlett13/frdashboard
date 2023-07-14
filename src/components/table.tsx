a00a8fe350f8ed9111a1f49566b4c4f8be94e46e
yudhapaderip97 <yudha@visiglobalteknologi.co.id>
2023-07-14 19:25:35 +0700

    perbaikan di table

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -1,13 +1,10 @@
 function TableLogAccess() {
   const data = [
     {
-      id: 1,
-      date: "2023-07-10",
-      message: "Log entry 1",
+      id: 2,
+      date: "2023-07-09",
+      message: "Log entry 2",
       image: "test1",
-      iscorrect: true,
-      isclear: true,
-      accuracylogerror: "string",
       accuracyaccesslog: "string",
     },
     {
@@ -15,19 +12,13 @@ function TableLogAccess() {
       date: "2023-07-09",
       message: "Log entry 2",
       image: "test1",
-      iscorrect: true,
-      isclear: true,
-      accuracylogerror: "string",
       accuracyaccesslog: "string",
     },
     {
-      id: 3,
-      date: "2023-07-08",
-      message: "Log entry 3",
+      id: 2,
+      date: "2023-07-09",
+      message: "Log entry 2",
       image: "test1",
-      iscorrect: false,
-      isclear: false,
-      accuracylogerror: "string",
       accuracyaccesslog: "string",
     },
   ];
@@ -41,11 +32,6 @@ function TableLogAccess() {
             <th className="py-2 px-4 bg-gray-200 text-left">Date</th>
             <th className="py-2 px-4 bg-gray-200 text-left">Message</th>
             <th className="py-2 px-4 bg-gray-200 text-left">Image</th>
-            <th className="py-2 px-4 bg-gray-200 text-left">Is Correct</th>
-            <th className="py-2 px-4 bg-gray-200 text-left">Is Clear</th>
-            <th className="py-2 px-4 bg-gray-200 text-left">
-              Accuracy Log Error
-            </th>
             <th className="py-2 px-4 bg-gray-200 text-left">
               Accuracy Access Log
             </th>
@@ -58,9 +44,6 @@ function TableLogAccess() {
               <td className="py-2 px-4 border">{log.date}</td>
               <td className="py-2 px-4 border">{log.message}</td>
               <td className="py-2 px-4 border">{log.image}</td>
-              <td className="py-2 px-4 border">{String(log.iscorrect)}</td>
-              <td className="py-2 px-4 border">{String(log.isclear)}</td>
-              <td className="py-2 px-4 border">{log.accuracylogerror}</td>
               <td className="py-2 px-4 border">{log.accuracyaccesslog}</td>
             </tr>
           ))}
@@ -73,34 +56,22 @@ function TableLogAccess() {
 function TableLogError() {
   const data = [
     {
-      id: 1,
-      date: "2023-07-14",
-      message: "Log entry 1",
-      image: "test1",
+      idaccesslog: 1,
       iscorrect: true,
       isclear: true,
       accuracylogerror: "string",
-      accuracyaccesslog: "string",
     },
     {
-      id: 2,
-      date: "2023-07-09",
-      message: "Log entry 2",
-      image: "test1",
+      idaccesslog: 2,
       iscorrect: true,
       isclear: true,
       accuracylogerror: "string",
-      accuracyaccesslog: "string",
     },
     {
-      id: 3,
-      date: "2023-07-08",
-      message: "Log entry 3",
-      image: "test1",
-      iscorrect: false,
-      isclear: false,
+      idaccesslog: 3,
+      iscorrect: true,
+      isclear: true,
       accuracylogerror: "string",
-      accuracyaccesslog: "string",
     },
   ];
 
@@ -109,31 +80,21 @@ function TableLogError() {
       <table className="min-w-full border-collapse border-black border-2">
         <thead className="border-black border-2">
           <tr>
-            <th className="py-2 px-4 bg-gray-200 text-left">ID</th>
-            <th className="py-2 px-4 bg-gray-200 text-left">Date</th>
-            <th className="py-2 px-4 bg-gray-200 text-left">Message</th>
-            <th className="py-2 px-4 bg-gray-200 text-left">Image</th>
+            <th className="py-2 px-4 bg-gray-200 text-left">ID Access Log</th>
             <th className="py-2 px-4 bg-gray-200 text-left">Is Correct</th>
             <th className="py-2 px-4 bg-gray-200 text-left">Is Clear</th>
             <th className="py-2 px-4 bg-gray-200 text-left">
               Accuracy Log Error
-            </th>
-            <th className="py-2 px-4 bg-gray-200 text-left">
-              Accuracy Access Log
             </th>
           </tr>
         </thead>
         <tbody border-black border-2>
           {data.map((log) => (
             <tr key={log.id}>
-              <td className="py-2 px-4 border">{log.id}</td>
-              <td className="py-2 px-4 border">{log.date}</td>
-              <td className="py-2 px-4 border">{log.message}</td>
-              <td className="py-2 px-4 border">{log.image}</td>
+              <td className="py-2 px-4 border">{log.idaccesslog}</td>
               <td className="py-2 px-4 border">{String(log.iscorrect)}</td>
               <td className="py-2 px-4 border">{String(log.isclear)}</td>
               <td className="py-2 px-4 border">{log.accuracylogerror}</td>
-              <td className="py-2 px-4 border">{log.accuracyaccesslog}</td>
             </tr>
           ))}
         </tbody>
---
+++ src/components/tableaccuracyerror.tsx
@@ -0,0 +1,25 @@
+export default function TableAccuracyError() {
+    const data = [
+        {
+            idaccesslog: 1,
+            iscorrect: true,
+            isclear: true,
+            accuracylogerror: "string",
+          },
+          {
+            idaccesslog: 1,
+            iscorrect: true,
+            isclear: true,
+            accuracylogerror: "string",
+          },
+          {
+            idaccesslog: 1,
+            iscorrect: true,
+            isclear: true,
+            accuracylogerror: "string",
+          },
+    ]
+    return (
+
+    )
+}
